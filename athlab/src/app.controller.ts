d016d78f0cdb1ee676ae64b281a8f5e5f24be4b4
Ognjen <0gnjen1@protonmail.com>
2023-05-15 14:57:28 +0200

    Clean up

--- athlab/src/app.controller.ts
+++ athlab/src/app.controller.ts
@@ -1,13 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { Admin } from '@prisma/client';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): Promise<Admin[]> {
-    return this.appService.getHello();
+  getHello(): void {
+    return 
   }
 }
--- athlab/src/app.service.ts
+++ athlab/src/app.service.ts
@@ -1,17 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { Admin, PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class AppService {
-  async getHello(): Promise<Admin[]> {
-    const prisma = new PrismaClient()
-    const admin = await prisma.admin.create({
-      data: {
-        email: "admin2@gmail.com",
-        name: "Donnie",
-        password: "not hashed lol"
-      }
-    });
-    return prisma.admin.findMany();
+  getHello(): void {
+    return
   }
 }
