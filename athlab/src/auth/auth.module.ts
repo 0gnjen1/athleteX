37fe9d03d71ee1757df4e9e0d020d1574362a9bc
Ognjen <0gnjen1@protonmail.com>
2023-05-16 14:39:34 +0200

    JwtGuard class implemented

--- athlab/src/auth/auth.module.ts
+++ athlab/src/auth/auth.module.ts
@@ -6,18 +6,19 @@ import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { LocalAthleteStrategy } from './strategies/local.athlete.strategy';
 import { LocalCoachStrategy } from './strategies/local.coach.strategy';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   providers: [AuthService,
               PrismaService,
               LocalAthleteStrategy,
-              LocalCoachStrategy],
+              LocalCoachStrategy,
+              JwtStrategy],
   imports: [PassportModule,
             JwtModule.register({
               secret: process.env.JWTKEY,
               signOptions: { expiresIn: '2592000s' } // 30 days
             })],
-  controllers: [AuthController],
-  exports: [AuthService]
+  controllers: [AuthController]
 })
 export class AuthModule {}
