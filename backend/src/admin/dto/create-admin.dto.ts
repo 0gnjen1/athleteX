03fbcff82008b89e183a7c588397a2051fc56798
Ognjen <0gnjen1@protonmail.com>
2023-08-07 01:02:24 +0200

    - Admin: Implemented class validator for create-admin DTO

--- backend/src/admin/dto/create-admin.dto.ts
+++ backend/src/admin/dto/create-admin.dto.ts
@@ -1,6 +1,17 @@
+import { IsAlpha, IsEmail, IsString, MinLength } from "class-validator";
+
 export class CreateAdminDto {
+
+    @IsAlpha()
     name: string;
+    
+    @IsEmail()
     email: string;
+
+    @MinLength(8)
     password: string;
+
+    @IsString()
     key: string;
+
 }
