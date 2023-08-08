44e0ea75fced9a69ebc003b14dc78018702dfdf4
Ognjen <0gnjen1@protonmail.com>
2023-08-08 23:35:40 +0200

    - Notifications: Update method implemented

--- backend/src/notifications/notifications.service.ts
+++ backend/src/notifications/notifications.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { UpdateNotificationDto } from '../dtos/notifications/update-notification.dto';
 import { PrismaService } from '../prisma/prisma.service';
 
@@ -77,7 +77,31 @@ export class NotificationsService {
     }
 
     async update(coachId: number, notificationId: number, updateNotificationDto: UpdateNotificationDto) {
-        
+        const coach = await this.prisma.coach.findUnique({
+            where: {
+                id: coachId
+            },
+            select: {
+                id: true
+            }
+        });
+        if(coach === null) throw new BadRequestException();
+        const notification  = await this.prisma.notifications.findUnique({
+            select: {
+                coach_id: true
+            },
+            where: {
+                id: notificationId
+            }
+        });
+        if(notification === null) throw new BadRequestException();
+        if(coachId !== notification.coach_id) throw new UnauthorizedException();
+        return await this.prisma.notifications.update({
+            data: updateNotificationDto,
+            where: {
+                id: notificationId
+            }
+        });
     }
 
     async remove(coachId: number, notificationId: number) {
