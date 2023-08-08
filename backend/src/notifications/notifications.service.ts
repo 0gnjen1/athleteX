779a23510fe6006e8b62cfcd6267558d8efb9930
Ognjen <0gnjen1@protonmail.com>
2023-08-08 23:28:53 +0200

    - Notifications: Get all and get by ID methods implemented

--- backend/src/notifications/notifications.service.ts
+++ backend/src/notifications/notifications.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { UpdateNotificationDto } from '../dtos/notifications/update-notification.dto';
 import { PrismaService } from '../prisma/prisma.service';
 
@@ -17,7 +17,7 @@ export class NotificationsService {
             }
         });
         if(coach === null) throw new BadRequestException();
-        return this.prisma.notifications.create({
+        return await this.prisma.notifications.create({
             data: {
                 title: title,
                 content: content,
@@ -27,11 +27,53 @@ export class NotificationsService {
     }
 
     async findAll(coachId: number, page: number, pgsize: number) {
-        
+        const coach = await this.prisma.coach.findUnique({
+            where: {
+                id: coachId
+            },
+            select: {
+                id: true
+            }
+        });
+        if(coach === null) throw new NotFoundException();
+        return await this.prisma.notifications.findMany({
+            skip: (page-1)*pgsize,
+            take: pgsize,
+            select: {
+                id: true,
+                title: true,
+                content: true,
+                coach_id: true
+            },
+            where: {
+                coach_id: coachId
+            }
+        });
     }
 
     async findOne(coachId: number, notificationId: number) {
-        
+        const coach = await this.prisma.coach.findUnique({
+            where: {
+                id: coachId
+            },
+            select: {
+                id: true
+            }
+        });
+        if(coach === null) throw new NotFoundException();
+        const notification = await this.prisma.notifications.findUnique({
+            select: {
+                id: true,
+                title: true,
+                content: true,
+                coach_id: true
+            },
+            where: {
+                id: notificationId
+            }
+        });
+        if(notification === null) throw new NotFoundException();
+        return notification;
     }
 
     async update(coachId: number, notificationId: number, updateNotificationDto: UpdateNotificationDto) {
