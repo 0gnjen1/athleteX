9c172bf8760dff9e4c5987975e3d04eba4d523fe
Ognjen <0gnjen1@protonmail.com>
2023-08-14 18:30:08 +0200

    - Injuries: Create method in the service

--- backend/src/injuries/injuries.service.ts
+++ backend/src/injuries/injuries.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class InjuriesService {}
+export class InjuriesService {
+
+    constructor(private readonly prisma: PrismaService){}
+
+    async createInjury(type: string){
+        return await this.prisma.injury.create({
+            data: {
+                type: type
+            }
+        });
+    }
+
+}
