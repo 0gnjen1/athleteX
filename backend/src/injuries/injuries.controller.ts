2457a11fac3fcb9ec850483d759ff63ef4e70330
Ognjen <0gnjen1@protonmail.com>
2023-08-15 18:38:06 +0200

    - Injuries: All methods implemented in controller

--- backend/src/injuries/injuries.controller.ts
+++ backend/src/injuries/injuries.controller.ts
@@ -1,4 +1,73 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Query, UnauthorizedException, UseGuards } from '@nestjs/common';
+import { InjuriesService } from './injuries.service';
+import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
+import { User } from 'src/decorators/users/user-from-req.decorator';
+import { CreateInjuryDto } from 'src/dtos/injuries/create-injury.dto';
+import { PagePipe } from 'src/pipes/pagination/page.pipe';
+import { PageSizePipe } from 'src/pipes/pagination/page-size.pipe';
+import { UpdateInjuryDto } from 'src/dtos/injuries/update-injury.dto';
 
 @Controller('injuries')
-export class InjuriesController {}
+export class InjuriesController {
+
+    constructor(
+        private readonly injuriesService: InjuriesService
+    ){}
+
+    @UseGuards(JwtAuthGuard)
+    @Get()
+    async findAllInjuries(
+        @Query('page', PagePipe) page: number,
+        @Query('pgsize', PageSizePipe) pgsize: number
+    ){
+        return await this.injuriesService.getAllInjuries(
+            page,
+            pgsize
+        );
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Get(':injuryid')
+    async findOneInjury(
+        @Param('injuryid', ParseIntPipe) injuryQueryId: number
+    ){
+        return await this.injuriesService.getInjuryById(
+            injuryQueryId
+        );
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Post()
+    async createInjury(
+        @User() user,
+        @Body() createInjuryDto: CreateInjuryDto
+    ){  
+        if(user.type !== "admin") throw new UnauthorizedException();
+        return await this.injuriesService.createInjury(
+            createInjuryDto.type
+        );
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Patch(":injuryid")
+    async updateInjury(
+        @User() user,
+        @Param('injuryid', ParseIntPipe) injuryQueryId: number,
+        @Body() updateInjuryDto: UpdateInjuryDto
+    ){
+        if(user.type !== "admin") throw new UnauthorizedException();
+        return await this.injuriesService.updateInjury(injuryQueryId, updateInjuryDto.type);
+    }
+
+
+    @UseGuards(JwtAuthGuard)
+    @Delete(":injuryid")
+    async removeInjury(
+        @User() user,
+        @Param('injuryid', ParseIntPipe) injuryQueryId: number,
+    ){
+        if(user.type !== "admin") throw new UnauthorizedException();
+        await this.injuriesService.removeInjury(injuryQueryId);
+    }
+
+}
